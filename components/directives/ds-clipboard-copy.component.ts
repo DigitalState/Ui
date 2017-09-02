da63cd33331d98b78ac7c1ad0b6c02a60a412bb7
Bushra Addlan <bushra.addlan@gmail.com>
2017-09-01 20:47:37 -0400

    Updated the Clipboard-Copy feature of UUIDs to show the icon onMouseOver a show a checkmark when clicking the icon.

--- components/directives/ds-clipboard-copy.component.ts
+++ components/directives/ds-clipboard-copy.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostListener, Input, ElementRef } from '@angular/core';
 
 import { ClipboardService } from './clipboard.service';
 
 @Component({
     selector: 'ds-clipboard-copy',
-    template: `<a class="btn btn-icon fa fa-clipboard" 
-                  title="{{ 'general.copyToClipboard' | translate }}" 
-                  ngxClipboard [cbContent]="copyContent"></a>`,
+    template: `
+        <span class="clipboard-target">
+            <ng-content></ng-content>
+            <a (click)="onClick()" class="btn btn-icon fa fa-clipboard fade position-relative" 
+               title="{{ 'general.copyToClipboard' | translate }}" 
+               ngxClipboard [cbContent]="copyContent">
+                <i class="fa fa-check animated fade text-success position-absolute position-right-0 v-center"></i>
+            </a>
+        </span>`,
     host: {
         class: 'ds-clipboard-copy'
     }
@@ -14,7 +20,31 @@ import { ClipboardService } from './clipboard.service';
 export class DsClipboardCopyComponent {
     @Input() public copyContent: string;
 
+
+    constructor(private elementRef:ElementRef) {
+
+    }
+
     ngOnInit(): void {
 
+    }
+
+    @HostListener('mouseover')
+    protected onMouseOver() {
+        jQuery(this.elementRef.nativeElement).find('.fa-clipboard').addClass('show');
+    }
+
+    @HostListener('mouseout')
+    protected onMouseOut() {
+        jQuery(this.elementRef.nativeElement).find('.fa-clipboard').removeClass('show');
+    }
+
+    protected onClick() {
+        const $checkMark = jQuery(this.elementRef.nativeElement).find('.fa-check');
+        $checkMark.addClass('fadeIn');
+
+        setTimeout(() => {
+            $checkMark.removeClass('fadeIn');
+        }, 3000);
     }
 }
